aa77aa3b63d33ac0990cbc9f23a13b76988a8fad
Percivalhluvukani <165777218+Percivalhluvukani@users.noreply.github.com>
2024-06-11 12:09:59 +0200

    Update New.py

--- New.py
+++ New.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import pickle
-import numpy 
+import numpy
 
 
 background_image_url = "https://cdn.britannica.com/16/187216-131-FB186228/tomatoes-tomato-plant-Fruit-vegetable.jpg"
@@ -38,13 +38,15 @@ def main():
     # Display the list of features
     #st.subheader('Features:')
     #'Weight_Kg', 'Low_Price', 'High_Price', 'Province', 'Container', 'Month'
-    features = ['Weight in Kg', 'Low Price in Rands', 'High Price in Rands', 'Province', 'Container', 'Month']
+    #Weight in Kg', 'Low Price in Rands', 'High Price in Rands ', 'Province', 'Container', 'Size_Grade', 'Month'
+    
+    features = ['Weight in Kg', 'Low Price in Rands', 'High Price in Rands', 'Province', 'Container','Size Grade', 'Month']
     #features = ['Weight_Kg', 'Low_Price', 'High_Price','Month_encoded', 'Province_encoded', 'Container_encoded','Size_Grade_encoded']
     
     month = ["April","August","February","December","January","July","June","March","May","October","September"]
     province = ["NATAL","NORTH EASTERN CAPE","TRANSVAAL"]
     container = ["BM050","BS060","BT070"]
-    #sizegrade = ["1L", "1M","1R","1S","1U","1X","1Z","2L","2M","2R","2S","2X","2Z","3M","3R","3S","3Z"]
+    sizegrade = ["1L", "1M","1R","1S","1U","1X","1Z","2L","2M","2R","2S","2X","2Z","3M","3R","3S","3Z"]
     
     user_inputs = {}
     for feature in features:
@@ -53,6 +55,8 @@ def main():
                 ss = province
             elif feature == "Container":
                 ss = container
+            elif feature == "Size Grade":
+                ss = sizegrade
             elif feature == "Month":
                 ss = month
     
@@ -80,7 +84,7 @@ def main():
         # Predict using the loaded model
         try:
             prediction = model.predict(user_inputs_df)
-            st.write('Predicting average price of Tomato-long life: R  ', prediction[0])
+            st.write('Predicting average price of Tomato-long life : R', prediction[0])
         except Exception as e:
             st.write("Error occurred during prediction:", e)
 
